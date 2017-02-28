c5479295ba747cc902f9e93b8cbea0a8f568a3e1
Yunhyeok Lee <yunhyeoklee@Yunhyeoks-MacBook-Pro.local>
2017-02-28 12:02:14 -0600

    Feb 28 Updated

--- As2/Comp_v2.js
+++ As2/Comp_v2.js
@@ -1,7 +1,7 @@
 /***************************************
 Name : Yunhyeok Lee
 Instructor : George Hamer
-Due Date : Feb 20 2017
+Due Date : Feb 22 2017
 Class : Complier CSC 446
 ***************************************/
 // To Test..  node Comp_v2.js Test.txt
@@ -11,7 +11,7 @@ Get file name from console to check data
 */
 var result = 0;
 
-  for (var i = 2; i < process.argv.length; i++) {
+for (var i = 2; i < process.argv.length; i++) {
     result += Number(process.argv[i]);
 }
 
@@ -19,7 +19,6 @@ var procedure_match = (/PROCEDURE/gi);
 var end_match = (/END/gi);
 var IS_match = (/IS/gi);
 var typeMark = (/integer|real|char|constant/gi);
-   // var declar_pick = (/PROCEDURE|END|BEGIN|/gi);
 function recursiveDescentParser () {
 
 var fs = require('fs');
@@ -30,9 +29,12 @@ var text = data.toString();
 var lines = text.split('\n');
 var procedure_tmp = text.split(' ');
 var procedure_count = 0;
-
+var idt_Error = (/CONSTANT|MODULE|BEGIN|PROCEDURE|IS|IF|THEN|ELSE|ELSIF|WHILE|LOOP|FLOAT|INTEGER|CHAR|CHAR|GET|PUT|END|--|[;]|[:]|[()]|[=]|[<]|[>]|[<=]|[>=]|[*]|[/]|rem|and|and|or/gi);
+var comments = (/-- */);
+var rel_ops = /[;]|[:]|[()]|[=]|[<]|[>]|[<=]|[>=]/gi;
+var mul_ops = /[*]|[/]|rem|and|and|or\B/gi;
 //Check the procedure how many procedure is working now
-  for(var i = 0; i < lines.length; i++) 
+  for(var i = 0; i < lines.length; i++)
     if(lines[i].match(procedure_match) != null)
            procedure_count++;
 //begin end check
@@ -41,6 +43,9 @@ var procedure_count = 0;
       if(lines[i].match(procedure_match) != null) {
         tmp_line = lines[i].split(' ');
         idt_check = tmp_line[1];
+         if(idt_check.match(idt_Error) !=null) {
+            console.log("Error Reserved word is in idt position");
+         }
       }
     if(lines[i].match(end_match) != null) {
         tmp_line = lines[i].split(' ');
@@ -48,7 +53,7 @@ var procedure_count = 0;
         end_check = tmp_line2[0];
       }
     }
-    if(idt_check.match(end_check) == null) 
+    if(idt_check.match(end_check) == null)
       console.log("idt and end is not matched");
   }
 
@@ -56,6 +61,9 @@ var procedure_count = 0;
      if(lines[0].match(procedure_match) != null) {
         tmp_line2 = lines[0].split(' ');
         idt_check2 = tmp_line2[1];
+        if(idt_check2.match(idt_Error) !=null) {
+            console.log("Error Reserved word is in idt position");
+         }
       }
     for(var i = 0; i< lines.length; i++) {
       if(lines[i].match(end_match) != null) {
@@ -64,7 +72,7 @@ var procedure_count = 0;
         end_check = tmp_line2[0];
       }
     }
-    if(idt_check2.match(end_check) == null) 
+    if(idt_check2.match(end_check) == null)
       console.log("idt and end is not matched1");
 
     for(var i = 1; i < (lines.length-3); i++) {
@@ -80,13 +88,13 @@ var procedure_count = 0;
       }
     }
 
-    if(idt_check2.match(end_check2) == null) 
+    if(idt_check2.match(end_check2) == null)
       console.log("idt and end is not matched2");
   }
- 
- // match_procedure start 
-   lines.forEach(function(line) {   
-          match_procedure(line); 
+
+ // match_procedure start
+   lines.forEach(function(line) {
+          match_procedure(line);
     })
 
     //declarativePart
@@ -96,34 +104,33 @@ var procedure_count = 0;
             if(lines2[0].match(/BEGIN/gi) != null) {
                check_begin = i+1;
                break;
-      } // if macth 
-    } //for loop 
-          for( var j = 0; j < check_begin; j++) {
-              var line3_tmp = lines[j].split(' ');
-               if(/PROCEDURE|END|BEGIN/gi.test(line3_tmp[0])!=true) {
-                 for(var k = 0; k < line3_tmp.length; k++) {
-                  var n = line3_tmp[k].search(/:/g);
-                    if( n >= 0) {
-                      if(/integer|real|char/gi.test(line3_tmp[k-1])==true) {
-                      console.log("Declarative Error " + line3_tmp[k-1]);
-                  }
-                }
-                  var n2 = line3_tmp[k].search(/:=/g);
-                    if( n2 >= 0) {
-                       if(/constant/gi.test(line3_tmp[k-1])!=true) {
-                      console.log("Declarative Error " + line3_tmp[k-1]);
-                  }
-                  if(/[0-9]/.test(line3_tmp[k+1])!=true) {
-                      console.log("Declarative Error " + line3_tmp[k+1]);
-                  }
-                }
+      } // if macth
+    } //for loop
+      for( var j = 0; j < check_begin; j++) {
+          var line3_tmp = lines[j].split(' ');
+            if(/PROCEDURE|END|BEGIN/gi.test(line3_tmp[0])!=true) {
+              for(var k = 0; k < line3_tmp.length; k++) {
+              var n = line3_tmp[k].search(/:/g);
+                if( n >= 0) {
+                  if(/integer|real|char/gi.test(line3_tmp[k-1])==true) {
+                  console.log("Declarative Error " + line3_tmp[k-1]);
               }
-            }              
+            }
+              var n2 = line3_tmp[k].search(/:=/g);
+                if( n2 >= 0) {
+                    if(/constant/gi.test(line3_tmp[k-1])!=true) {
+                  console.log("Declarative Error " + line3_tmp[k-1]);
+              }
+              if(/[0-9]/.test(line3_tmp[k+1])!=true) {
+                  console.log("Declarative Error " + line3_tmp[k+1]);
+              }
+            }
           }
-         //DeclarativePart Finish          
+        }
+      }
+      //DeclarativePart Finish
   })
 }
-
 // Procedure Match
 // Here is check about idt Args is
 function match_procedure(line) {
@@ -134,7 +141,7 @@ function match_procedure(line) {
       var mode_check2 = 0; // ; check
       var mode_check3 = 0; // : check
       var mode_check4 = 0; // ; check
-            
+
       //Is check
      if(lines2[0].match(procedure_match) != null) {
       lines2.forEach(function(line_ISCHECK) {
@@ -142,18 +149,16 @@ function match_procedure(line) {
           is_check++;
         }
       })
-
       //Is Checking
       if(is_check < 1) {
         console.log("IS is missing\n");
         console.log(lines2);
         is_check = 0;
       }
-
-      //Paran Check 
+      //Paran Check
       for(var i = 0; i < line.length; i++) {
        //console.log(line[i]);
-      if(line[i] == "(") { 
+      if(line[i] == "(") {
           paran_check++;
           mode_check = i;
       }
@@ -161,7 +166,7 @@ function match_procedure(line) {
           paran_check++;
       if(line[i] == ";") {
           mode_check4++;
-          mode_check2 = i; // this need to make Args 
+          mode_check2 = i; // this need to make Args
       }
        if(line[i] == ":")
           mode_check3++;
@@ -172,12 +177,10 @@ function match_procedure(line) {
       }
       tmp = mode_check3 - mode_check4;
 
-      if(mode_check4 >= mode_check3 && paran_check > null) 
+      if(mode_check4 >= mode_check3 && paran_check > null)
         console.log(line + ": or ; missing");
-
   }
 }
-
 
 /*
 Checking ID, Reserved_words, operator, Special Value
@@ -186,8 +189,8 @@ function LexicalAnalyzer () {
   /*
   Input File into this program to split each line!
   */
-var fs = require('fs');
-fs.readFile(process.argv[2],function(err,data) {
+  var fs = require('fs');
+  fs.readFile(process.argv[2],function(err,data) {
     if(err) throw err;
     var reserved_words = (/CONSTANT|MODULE|BEGIN|PROCEDURE| IS|IF|THEN|ELSE|ELSIF|WHILE|LOOP|FLOAT|INTEGER|CHAR|CHAR|GET|PUT|END/gi);
     var comments = (/-- */);
@@ -197,7 +200,6 @@ fs.readFile(process.argv[2],function(err,data) {
     var result = ['reserve word is.. '];
     var text = data.toString();
     var lines = text.split('\n');
-
     var comment = [];
     var rel_op =[];
     // lines.forEach(function(line) {
@@ -209,9 +211,9 @@ fs.readFile(process.argv[2],function(err,data) {
 
       // Checking Reserved_word
     lines.forEach(function(line) {
-         if(line.match(reserved_words) !=null) {
-           result.push(line.match(reserved_words));
-         }
+          if(line.match(reserved_words) !=null) {
+            result.push(line.match(reserved_words));
+          }
     })
 
     //console.log(result);
@@ -228,7 +230,7 @@ fs.readFile(process.argv[2],function(err,data) {
             comment.push(line + ' <-- comment');
         }
     })
-     // console.log(comment);
+      // console.log(comment);
 
     //Relational Operator
   //  console.log('relational op is.. ');
@@ -279,13 +281,13 @@ fs.readFile(process.argv[2],function(err,data) {
 
       var pattern1='"';
       var check_que;
-       for(var i = 0; i < lines.length; i++) {
+        for(var i = 0; i < lines.length; i++) {
           for( var j = 0; j <lines[i].length; j++) {
             if(lines[i][j] == pattern1)
                 check_que = check_que + 1;
           }
           if(check_que == 1)
-           // console.log("Unchecked Que mark");
+            // console.log("Unchecked Que mark");
             check_que = 0;
         }
 
@@ -293,21 +295,17 @@ fs.readFile(process.argv[2],function(err,data) {
       var Lparanthesis = '(';
       var Rparanthesis = ')';
       for(var i = 0; i < lines.length; i++) {
-         for( var j = 0; j <lines[i].length; j++) {
-           if(lines[i][j] == Lparanthesis) {
+          for( var j = 0; j <lines[i].length; j++) {
+            if(lines[i][j] == Lparanthesis) {
               if(lines[i][j] == Rparanthesis || lines[i][j] == Lparanthesis) {
                 check_paran = check_paran + 1; //Two is matched Correctly
             }
-         }
-       }
+          }
+        }
     }
 
     })
   }
 
-
 recursiveDescentParser ();
 LexicalAnalyzer();
-
-
-
